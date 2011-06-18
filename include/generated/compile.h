a0ab90325f32f81b2b2c8e00f42a28ddae42326d
Clemsyn <clemsyn@clemsyn-MM061.(none)>
2011-06-17 21:33:38 -0500

    first commit for updated kernel

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 289 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#289 SMP PREEMPT Fri Jun 17 20:57:50 CDT 2011"
+#define LINUX_COMPILE_TIME "20:57:50"
+#define LINUX_COMPILE_BY "clemsyn"
+#define LINUX_COMPILE_HOST "clemsyn-MM061"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
